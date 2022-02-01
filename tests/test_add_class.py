cdb5e1546316684817b1f749d232d2d325fecdcb
David Gardner <dagardner@nvidia.com>
2022-02-01 01:00:07 +0000

    Tests for AddClassificationsStage

--- tests/test_add_class.py
+++ tests/test_add_class.py
@@ -16,6 +16,9 @@
 
 import os
 import unittest
+from unittest import mock
+
+import cupy as cp
 
 from morpheus.config import Config
 
@@ -25,6 +28,7 @@ from morpheus.config import ConfigAutoEncoder
 from morpheus.config import PipelineModes
 from morpheus.pipeline import LinearPipeline
 from morpheus.pipeline.general_stages import AddClassificationsStage
+from morpheus.pipeline.messages import MultiResponseProbsMessage
 from tests import BaseMorpheusTest
 
 
@@ -42,6 +46,7 @@ class TestAddClass(BaseMorpheusTest):
         self.assertEqual(ac._class_labels, ['frogs', 'lizards', 'toads'])
         self.assertEqual(ac._labels, ['frogs', 'lizards', 'toads'])
         self.assertEqual(ac._idx2label, {0: 'frogs', 1: 'lizards', 2: 'toads'})
+        self.assertEqual(ac.name, "add-class")
 
         ac = AddClassificationsStage(config, threshold=1.3, labels=['lizards'], prefix='test_')
         self.assertEqual(ac._class_labels, ['frogs', 'lizards', 'toads'])
@@ -49,6 +54,45 @@ class TestAddClass(BaseMorpheusTest):
         self.assertEqual(ac._idx2label, {1: 'test_lizards'})
 
         self.assertRaises(AssertionError, AddClassificationsStage, config, labels=['missing'])
+
+    def test_add_labels(self):
+        mock_message = mock.MagicMock()
+        mock_message.probs = cp.array([[0.1, 0.5, 0.8], [0.2, 0.6, 0.9]])
+
+        config = Config.get()
+        config.mode = PipelineModes.FIL
+        config.use_cpp = False
+        config.class_labels = ['frogs', 'lizards', 'toads']
+
+        ac = AddClassificationsStage(config, threshold=0.5)
+        ac._add_labels(mock_message)
+
+        mock_message.set_meta.assert_has_calls([
+            mock.call('frogs', [False, False]),
+            mock.call('lizards', [False, True]),
+            mock.call('toads', [True, True]),
+        ])
+
+        wrong_shape = mock.MagicMock()
+        wrong_shape.probs = cp.array([[0.1, 0.5], [0.2, 0.6]])
+        self.assertRaises(RuntimeError, ac._add_labels, wrong_shape)
+
+    def test_build_single(self):
+        mock_stream = mock.MagicMock()
+        mock_segment = mock.MagicMock()
+        mock_segment.make_node.return_value = mock_stream
+        mock_input = mock.MagicMock()
+
+        config = Config.get()
+        config.mode = PipelineModes.FIL
+        config.use_cpp = False
+        config.class_labels = ['frogs', 'lizards', 'toads']
+
+        ac = AddClassificationsStage(config)
+        ac._build_single(mock_segment, mock_input)
+
+        mock_segment.make_node.assert_called_once()
+        mock_segment.make_edge.assert_called_once()
 
 
 if __name__ == '__main__':
