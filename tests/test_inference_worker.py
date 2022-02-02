b1859691dd241f922525972fb60298df972c552a
David Gardner <dagardner@nvidia.com>
2022-02-02 17:53:49 +0000

    First pass at unittesting the inference worker

--- tests/test_inference_worker.py
+++ tests/test_inference_worker.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 import asyncio
+import threading
 import unittest
 from unittest import mock
 
@@ -32,11 +33,30 @@ from tests import BaseMorpheusTest
 class TestInferenceWorker(BaseMorpheusTest):
     @mock.patch('asyncio.Event')
     @mock.patch('morpheus.pipeline.inference.inference_stage.IOLoop')
-    def test_constructor(self, mock_ioloop, mock_event):
+    def test_constructor(self, mock_ioloop, mock_event_cls):
+        mock_current_loop = mock.MagicMock()
+        mock_ioloop.current.return_value = mock_current_loop
+
+        mock_event = mock.MagicMock()
+        mock_event_cls.return_value = mock_event
+
         pq = ProducerConsumerQueue()
         iw = inference_stage.InferenceWorker(pq)
+        self.assertIs(iw._loop, mock_current_loop)
+        self.assertIsNone(iw._thread)
+        self.assertIs(iw._inf_queue, pq)
+        self.assertIs(iw._complete_event, mock_event)
 
+    @mock.patch("asyncio.Event.wait")
+    @mock.patch('threading.Thread')
+    def test_start(self, mock_thread, mock_event_wait):
+        mock_thread.return_value = mock_thread
 
+        pq = ProducerConsumerQueue()
+        iw = inference_stage.InferenceWorker(pq)
+        asyncio.run(iw.start())
+        mock_thread.start.assert_called_once()
+        mock_event_wait.assert_awaited()
 
 if __name__ == '__main__':
     unittest.main()
